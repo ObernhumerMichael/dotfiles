b032a27148511cd78944fdc2424ee63a51c1ee48
ObernhumerMichael <michaelobernhumer@gmail.com>
2024-11-26 13:29:04 +0100

    feat: sync rofi wallpaper with monitor wallpaper + added SUPER+R toggle

---
+++ hypr/scripts/open-rofi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if Rofi is running
+if pgrep -x rofi > /dev/null; then
+  pkill -x rofi
+else
+  monitor=$(hyprctl -j monitors | jq -r '.[] | select(.focused == true).name')
+  ln -sf ~/.cache/wallpaper-selector/$monitor ~/.cache/wallpaper-selector/rofi
+  rofi -config ~/.config/rofi/styles/style_01.rasi -show 
+fi
