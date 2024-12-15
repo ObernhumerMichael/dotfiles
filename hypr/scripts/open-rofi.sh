5679504584f5dbdf77a48ea88244eb4b08102a38
ObernhumerMichael <michaelobernhumer@gmail.com>
2024-12-15 20:27:19 +0100

    fix: [#5] scale images in rofi correctly

--- hypr/scripts/open-rofi.sh
+++ hypr/scripts/open-rofi.sh
@@ -6,5 +6,9 @@ if pgrep -x rofi > /dev/null; then
 else
     monitor=$(hyprctl -j monitors | jq -r '.[] | select(.focused == true).name')
     ln -sf ~/.cache/wallpaper-selector/$monitor ~/.cache/wallpaper-selector/rofi
+
+    # Resize to specific dimensions and keep the aspect ratio
+    magick ~/.cache/wallpaper-selector/$monitor -resize 700x600^ -gravity center -extent 700x600 ~/.cache/wallpaper-selector/scaled-rofi.png
+
     rofi -config ~/.config/rofi/styles/style_01.rasi -show
 fi
