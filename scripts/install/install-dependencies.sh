d5262554e79bbd0892fdb8114c0f6f1e5f55f62c
ObernhumerMichael <michaelobernhumer@gmail.com>
2024-12-22 17:51:09 +0100

    feat: [#6] open nmtui on fail + open nm-connection-editor on right click

--- scripts/install/install-dependencies.sh
+++ scripts/install/install-dependencies.sh
@@ -12,7 +12,7 @@ echo "Bluetooth related essentails"
 sudo pacman -S --needed bluez bluez-utils blueman
 
 echo "System basics"
-sudo pacman -S --needed openssh unzip nodejs npm brightnessctl wl-clipboard net-tools bc upower socat i2c-tools ddcutil cpio cmake bind imagemagick gdb
+sudo pacman -S --needed openssh unzip nodejs npm brightnessctl wl-clipboard net-tools bc upower socat i2c-tools ddcutil cpio cmake bind imagemagick gdb network-manager-applet
 # Basic system applications
 sudo pacman -S --needed gwenview neovim tmux thunar
 
